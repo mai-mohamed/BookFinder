80d3fc8f85aa027fcfe55262a1748a29a4e0bf03
mai mohamed <maimohamedag@gmail.com>
2020-09-03 10:41:20 +0200

    add header

---
+++ src/Components/BookFinder/BookHeader/BookHeader.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function BookHeader(){
+    return(
+        <div>
+            <h1>Welcome To Book Finder</h1>
+        </div>
+    )
+}
+export default BookHeader;
--- src/Views/BookFinder.jsx
+++ src/Views/BookFinder.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Search from "../Components/BookFinder/Search/Search";
 import BookList from "../Components/BookFinder/BookList/BookList";
+import BookHeader from "../Components/BookFinder/BookHeader/BookHeader";
 
 function BookFinder() {
   return (
     <div>
+      <BookHeader/>
       <Search />
       <BookList />
     </div>
